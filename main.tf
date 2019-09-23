1112d8c72af9b6f0229b6a7e0bd8b2eb2d109547
Mohammed A. El Meziani <mohammed.elmeziani@keepler.io>
2019-09-23 16:18:40 +0200

    Add principal files

--- main.tf
+++ main.tf
@@ -6,73 +6,23 @@ provider "aws" {
   region     = var.region
 }
 
-#create EC2 instance
-resource "aws_instance" "my_web_instance" {
-  ami                    = var.images[var.region]
-  instance_type          = "t2.micro"
-  key_name               = "key" #make sure you have your_private_ket.pem file
-  vpc_security_group_ids = [aws_security_group.web_security_group.id]
-  subnet_id              = aws_subnet.myvpc_public_subnet.id
-  tags = {
-    Name = "my_web_instance"
-  }
-  volume_tags = {
-    Name = "my_web_instance_volume"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo mkdir -p /var/www/html/", #install apache, mysql client, php
-      "sudo yum update -y",
-      "sudo yum install -y httpd",
-      "sudo service httpd start",
-      "sudo usermod -a -G apache ec2-user",
-      "sudo chown -R ec2-user:apache /var/www",
-      "sudo yum install -y mysql php php-mysql",
-    ]
-  }
-  provisioner "file" {
-    source = "index.php" #copy the index file form local to remote
-
-    destination = "/var/www/html/index.php"
-  }
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    host     = aws_instance.my_web_instance.public_dns
-
-    #copy <your_private_key>.pem to your local instance home directory
-    #restrict permission: chmod 400 <your_private_key>.pem
-    private_key = file("/Users/melmeziani/.ssh/key.pem")
-  }
+#network module
+module "network" {
+  source = "./modules/network"
 }
 
-#create aws rds subnet groups
-resource "aws_db_subnet_group" "my_database_subnet_group" {
-  name       = "mydbsg"
-  subnet_ids = [aws_subnet.myvpc_private_subnet_one.id, aws_subnet.myvpc_private_subnet_two.id]
-  tags = {
-    Name = "my_database_subnet_group"
-  }
+#instance module
+module "instance" {
+  source = "./modules/instance"
+  instance_region = var.region
+  web_security_group = [module.network.web_security_group]
+  public_subnet      = module.network.public_subnet
 }
 
-#create aws mysql rds instance
-resource "aws_db_instance" "my_database_instance" {
-  allocated_storage      = 20
-  storage_type           = "gp2"
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
-  port                   = 3306
-  vpc_security_group_ids = [aws_security_group.db_security_group.id]
-  db_subnet_group_name   = aws_db_subnet_group.my_database_subnet_group.name
-  name                   = "mydb"
-  identifier             = "mysqldb"
-  username               = "myuser"
-  password               = "mypassword"
-  parameter_group_name   = "default.mysql5.7"
-  skip_final_snapshot    = true
-  tags = {
-    Name = "my_database_instance"
-  }
+#rds module
+module "rds" {
+  source = "./modules/rds"
+  db_security_group  = [module.network.db_security_group]
+  private_subnet_one = module.network.private_subnet_one
+  private_subnet_two = module.network.private_subnet_two
 }
-
