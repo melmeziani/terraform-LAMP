5e578e8aa0fddbb05828821757027966778253c5
Mohammed A. El Meziani <mohammed.elmeziani@keepler.io>
2019-09-23 15:32:28 +0200

    Separate in modules, add network module

---
+++ modules/network/output.tf
@@ -0,0 +1,19 @@
+output "public_subnet" {
+  value = aws_subnet.myvpc_public_subnet.id
+}
+
+output "private_subnet_one" {
+  value = aws_subnet.myvpc_private_subnet_one.id
+}
+
+output "private_subnet_two" {
+  value = aws_subnet.myvpc_private_subnet_two.id
+}
+
+output "web_security_group" {
+  value = aws_subnet.aws_security_group.web_security_group.id
+}
+
+output "db_security_group" {
+  value = aws_subnet.aws_security_group.db_security_group.id
+}
--- output.tf
+++ output.tf
@@ -1,6 +1,8 @@
 output "db_server_address" {
-value = "${aws_db_instance.my_database_instance.address}"
+  value = aws_db_instance.my_database_instance.address
 }
+
 output "web_server_address" {
-value = "${aws_instance.my_web_instance.public_dns}"
+  value = aws_instance.my_web_instance.public_dns
 }
+
